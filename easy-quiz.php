13d540a573ec91d38cfd0c5c83982397f25eb53f
manojtd <manojtd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-15 00:25:13 +0000

    Committing V1.00: Initial release. [Sep 10, 2012]
    git-svn-id: https://plugins.svn.wordpress.org/easy-quiz/trunk@599139 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-quiz.php
@@ -0,0 +1,135 @@
+<?php
+/*
+  Plugin Name: Easy Quiz
+  Plugin URI: http://www.thulasidas.com/plugins/ezquiz
+  Description: <em>Lite Version</em>: Easiest Quiz Plugin ever. No Admin page, no options, just a shortcode on a page to create jQuery quiz!
+  Version: 1.00
+  Author: Manoj Thulasidas
+  Author URI: http://www.thulasidas.com
+*/
+
+/*
+  License: GPL2 or later
+  Copyright (C) 2008 www.thulasidas.com
+*/
+
+if (class_exists("ezQuiz")) {
+  // Another version is probably installed. Ask the user to deactivate it.
+  die (__("<strong><em>Easy Quiz:</em></strong> Another version of this plugin is active.<br />Please deactivate it before activating <strong><em>Easy Quiz</em></strong>.", "easy-adsenser"));
+}
+else {
+  class EzQuiz {
+    var $plgDir, $plgURL ;
+    const shortCode = 'ezquiz' ;
+    static $quizPage ;
+    function ezQuiz() { //constructor
+      $this->plgURL = plugins_url(basename(dirname(__FILE__))) ;
+      $this->plgDir = dirname (__FILE__) ;
+    }
+    static function findShortCode($posts) {
+      self::$quizPage = false ;
+      if (empty($posts)) return $posts ;
+      foreach ($posts as $post) {
+        if (stripos($post->post_content, self::shortCode) !== false) {
+          self::$quizPage = true ;
+          break ;
+        }
+      }
+      return $posts ;
+    }
+    function ezqStyles() {
+      if (!self::$quizPage) return ;
+      if (is_admin()) return ;
+      wp_register_style('ezQuizCSS', "{$this->plgURL}/jQuizMe.css") ;
+      wp_enqueue_style('ezQuizCSS') ;
+    }
+    function ezqScripts() {
+      if (!self::$quizPage) return ;
+      if (is_admin()) return ;
+      wp_register_script('ezQuizJS', "{$this->plgURL}/jQuizMe.js", array('jquery')) ;
+      wp_enqueue_script('ezQuizJS') ;
+    }
+    function process($content){
+      self::$quizPage = true ;
+      $lines = explode("\n", strip_tags($content)) ;
+      $title = "title: \"Easy Quiz\"," ;
+      $help = "help: \"Choose True or False. At the end of the quiz, you will get your score.\"," ;
+      $quiz = '<div id="quizArea">
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+$( function($){
+var quiz = [' ;
+      $comma = '' ;
+      foreach ($lines as $line) {
+        if (empty($line)) continue ;
+        $toOutput = true ;
+        @list($label, $rest) = explode(':', $line, 2) ;
+        if (strlen($label) > 10) {
+          $label = $rest = '' ;
+        }
+        $label = strtolower(trim($label)) ;
+        if (!empty($rest)) {
+          switch ($label) {
+          case 'q':
+          case 'ques' :
+          case 'question':
+            break ;
+          case 'a':
+          case 'ans' :
+          case 'answer':
+            break ;
+          case 'title' :
+            $title = 'title: "' . $rest . '",' ;
+            $toOutput = false ;
+            break ;
+          case 'help' :
+            $help = 'help: "' . $rest . '",' ;
+            $toOutput = false ;
+            break ;
+          default:
+          }
+        }
+        else {
+          $rest = $line ;
+        }
+        if ($toOutput) {
+          $quiz .= "$comma{ ques: \"$rest\", ans: \"\" }" ;
+          $comma = ',' ;
+        }
+      }
+      $quiz .= '];
+var options = {' .
+$title .
+$help .
+'showAns: false,
+showAnsInfo: false,
+quizType: "tf"
+};
+var lang = {
+quiz : {
+tfEqual:""
+}
+};
+$( "#quizArea" ).jQuizMe( quiz, options, lang );
+});
+})
+</script>
+</div>' ;
+      return $quiz ;
+    }
+    function displayQuiz($atts, $content='') {
+      $quiz = $this->process($content) ;
+      return $quiz ;
+    }
+  }
+} //End Class ezQuiz
+
+if (class_exists("ezQuiz")) {
+  $ezQuiz = new EzQuiz() ;
+  if (isset($ezQuiz)) {
+    add_shortcode(EzQuiz::shortCode, array(&$ezQuiz, 'displayQuiz')) ;
+    add_action('wp_enqueue_scripts' , array(&$ezQuiz, 'ezqStyles')) ;
+    add_action('wp_enqueue_scripts' , array(&$ezQuiz, 'ezqScripts')) ;
+    add_filter('the_posts', array("EzQuiz", "findShortCode")) ;
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,72 @@
+=== Easy Quiz ===
+Contributors: manojtd
+Donate link: http://affiliates.thulasidas.com
+Tags: quiz, survey, questions, education
+Requires at least: 3.1
+Tested up to: 3.3
+Stable tag: 1.20
+License: GPLv2 or later
+
+A quick and easy quiz plugin to present a set of questions to your reader and let them check the answers.
+
+== Description ==
+
+*Easy Quiz* is a quick and easy quiz plugin to present a set of questions to your reader and let them check the answers. The answers and the statistics are *not* stored in your database and the purpose of the quiz is purely your reader's entertainment. It creates quizzes vaguely similar to the [BBC Weekly Quiz](http://www.bbc.co.uk/news/magazine-19511527 "BBC Quiz").
+
+All the survey/quiz plugins I found in the directory looked too complex for my purpose. They were good, but were geared toward serious purposes like collecting information, education and tracking etc.  All I wanted to do was to create an interactive page for my readers to take a test for their own entertainment. I didn't want to store their info on my server, give statistical analysis etc. So I wrote this plugin based on the excellent jQuery script called [jQuizMe](http://code.google.com/p/jquizme/ "jQuizMe code page").
+
+= Features =
+
+1. No setup other than cutting and pasting your questions on your post.
+2. True or False type questions.
+3. Results without server submit.
+4. View results at any point, and continue the quiz.
+
+= Pro Version =
+
+A [pro version](http://buy.thulasidas.com "Pro Version of Eazy Quiz for $2.95") of this plugin is in the works with the following added features:
+
+1. Color customization to match your theme.
+2. Per quiz color overrides.
+3. More quiz types (multiple choice, fill in the blanks etc.)
+4. Other quiz options from the core JS exposed in the admin page and as short code options.
+
+== Upgrade Notice ==
+
+Initial release.
+
+== Installation ==
+
+You can install it using the WordPress Plugins -> Add New interface.
+
+Or,
+1. Upload the Easy Quiz plugin (the whole `easy-quiz` folder) to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in your blog.
+
+To use the plugin, use the shorttage `[ezquiz]`. In other words, enclose your quiz questions between `[ezquiz][/ezquiz]` tags on a page. See the FAQ for an example.
+
+== Screenshots ==
+
+1. Sample quiz.
+
+== Frequently Asked Questions ==
+
+= Why another quiz plugin? =
+
+This plugin is the simplest quiz plugin you can imagine. To use it, you include a set of statements between the shorttags `[ezquiz][/ezquiz]` in a post. The statements will be neatly rendered as a true or false quiz. Note that all the right answers are true! It will change in the near future.
+
+= Still not clear how to use it. An example please? =
+
+Create a new post on your test blog with the following content.
+`This is a quiz about the wonderful WordPress blogging platform.
+[ezquiz]
+title:WordPress is free and priceless
+WordPress is priceless.
+WordPress is free.
+[/ezquiz]
+If you agree with these statements, you are a good man.`
+Publish it and browse to the published page.
+
+== Change Log ==
+
+* V1.00: Initial release. [Sep 10, 2012]
